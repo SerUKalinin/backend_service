afddf991fb2a8c163d34da85fba89ac2a189941f
SerUKalinin <serukalinin@gmail.com>
2025-03-25 23:20:58 +0300

    Настройка CI

--- src/test/java/com/example/auth_service/AuthServiceApplicationTests.java
+++ src/test/java/com/example/auth_service/AuthServiceApplicationTests.java
@@ -2,8 +2,12 @@ package com.example.auth_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:/application-test.properties")
 class AuthServiceApplicationTests {
 
 	@Test
